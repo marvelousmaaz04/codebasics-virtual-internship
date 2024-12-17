4d302abee2b54b58270d59c4156a2b87ef3c6ea0
maaz <shaikhmaaz04@gmail.com>
2024-12-17 22:25:05 +0530

    Week 3 Started

--- Week_2/resources_4/exercise_prac1.sql
+++ Week_2/resources_4/exercise_prac1.sql
@@ -91,8 +91,10 @@ SELECT (SUM(evs_sold) * 100/SUM(tvs_sold)) as penetration_rate
 FROM(
 SELECT state, electric_vehicles_sold as evs_sold, 
 total_vehicles_sold as tvs_sold FROM 
-electric_vehicle_sales_by_state
-WHERE state="Chandigarh"
+electric_vehicle_sales_by_state evs
+JOIN dim_date dd
+ON dd.date=evs.date
+WHERE state="Chandigarh" and dd.fiscal_year=2024
 ) AS subquery;
 
 SELECT 
@@ -103,3 +105,19 @@ FROM (
     FROM electric_vehicle_sales_by_state
     WHERE LOWER(state) = "chandigarh" AND total_vehicles_sold > 0
 ) AS subquery;
+
+SELECT 
+    	evs.state,
+    	SUM(evs.electric_vehicles_sold) AS total_ev_sales,
+    	SUM(evs.total_vehicles_sold) AS total_vehicles_sold,
+    	SUM(evs.electric_vehicles_sold) / SUM(evs.total_vehicles_sold) * 100 AS penetration_rate,
+   	 CASE WHEN (SUM(evs.electric_vehicles_sold) / SUM(evs.total_vehicles_sold)) * 100 > 7 THEN "Above 7%"
+WHEN (SUM(evs.electric_vehicles_sold) / SUM(evs.total_vehicles_sold)) * 100 > 5 THEN 'Above 5%'
+WHEN (SUM(evs.electric_vehicles_sold) / SUM(evs.total_vehicles_sold)) * 100 > 3 THEN 'Above 3%'
+WHEN (SUM(evs.electric_vehicles_sold) / SUM(evs.total_vehicles_sold)) * 100 > 1 THEN 'Above 1%'
+        	ELSE 'Below 1%'
+    	END AS penetration_category
+FROM ev_sales_db.electric_vehicle_sales_by_state evs
+JOIN ev_sales_db.dim_date dd ON evs.date = dd.date
+WHERE dd.fiscal_year = 2024
+GROUP BY evs.state;
